664a9a92a4ec5c4816b5136616ba174381a166a6
compwron <wordymusic@gmail.com>
2013-06-08 17:19:22 -0500

    adding source and rspec to Gemfile

--- generator.sh
+++ generator.sh
@@ -2,10 +2,16 @@ mkdir bin
 mkdir lib
 mkdir spec
 mkdir config
+touch README.md
+
+# Gemfile
 touch Gemfile
+echo "source 'http://rubygems.org'" > Gemfile
+echo "gem 'rspec'" >> Gemfile
+
+# first Git commit
 touch .gitignore
 echo 2.0.0 > .ruby-version
-touch README.md
 git init
 git add .
 git commit -m "Project structure"
